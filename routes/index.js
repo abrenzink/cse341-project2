e20282a923bcb0d79884a813906ef3d3d01bf8cc
abrenzink <a.brenzink@gmail.com>
2023-09-20 23:43:32 -0300

    final changes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/', require('./swagger.js'));
+
 router.get('/', (req, res) => {
+    //#swagger.tags=['Hello world']
     res.send("Web services Project 2 - Andrea Brenzink");
 });
 
--- server.js
+++ server.js
@@ -9,6 +9,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-AllowHeaders',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader(
+        'Access-Control-Allow_methods', 
+        'GET, POST, PUT, DELETE, OPTIONS'
+    );
+    next();
+});
 app.use('/', require('./routes'));
 
 
